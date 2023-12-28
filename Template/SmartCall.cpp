3e085c80c2c26f7fdbf64e47ff6e9499356dec89
jovy.zhang <jovy.zhang@zoom.us>
2023-12-28 14:42:15 +0800

    (feature) 裸/智能指针安全调用成员函数并返回 [完善模板参数约束,支持返回void的成员函数]

--- Template/SmartCall.cpp
+++ Template/SmartCall.cpp
@@ -1,69 +1,109 @@
 //支持通过裸指针、shared_ptr、weak_ptr安全的调用成员函数
 
+#include <functional>
+#include <memory>
+#include <iostream>
+#include <string>
+
 class Test
 {
-    private:
-    int data = 100;
-    public:
-    Test() = default;
+	private:
+		int data = 100;
+	public:
+		Test() = default;
 
-    int test(int x)
-    {
-        return x + data;
-    }
+		int testint(int x)
+		{
+			return x + data;
+		}
+
+		void testvoid()
+		{
+			std::cout << "void \t";
+		}
+
+		void testoverload1(const std::string& str)
+		{
+			std::cout << "testoverload1(const string&) \t";
+		}
+
+		void testoverload1(std::string& str)
+		{
+			std::cout << "testoverload1(string&) \t";
+		}
+    
+        bool testbool()
+        {
+            std::cout << "bool testbool \t";
+            return true;
+        }
 };
 
-template <typename Caller, typename Callable, typename ...Args>
-auto SafeCall(const Caller* caller, Callable callable, Args ...args) -> decltype(auto)
+	template <typename Caller, typename Callable, typename ...Args>
+auto SafeCall(Caller* caller, Callable&& callable, Args&&...args) -> decltype(auto)
 {
-    using RetType = decltype((std::declval<Caller>().*callable)(std::forward<Args>(args)...));
+	using RetType = decltype((std::declval<Caller>().*std::forward<Callable>(callable))(std::forward<Args>(args)...));
 
-    if(nullptr == caller)   return RetType{};
+	if(nullptr == caller)   return RetType();
 
-    std::cout << __FUNCTION__ << "(raw):" << __LINE__ << ' ';
-    return (const_cast<Caller*>(caller)->*callable)(std::forward<Args>(args)...);
+	std::cout << __FUNCTION__ << "(raw):" << __LINE__ << ' ';
+	return (const_cast<Caller*>(caller)->*std::forward<Callable>(callable))(std::forward<Args>(args)...);
 }
 
-template <typename Caller, typename Callable, typename ...Args>
-auto SafeCall(std::weak_ptr<Caller> caller, Callable callable, Args ...args) -> decltype(auto)
+	template <typename Caller, typename Callable, typename ...Args>
+auto SafeCall(std::weak_ptr<Caller> caller, Callable&& callable, Args&&...args) -> decltype(auto)
 {
-    using RetType = decltype((std::declval<Caller>().*callable)(std::forward<Args>(args)...));
+	using RetType = decltype((std::declval<Caller>().*std::forward<Callable>(callable))(std::forward<Args>(args)...));
 
-    const auto &shareCaller = caller.lock();
-    if(!shareCaller)    return RetType{};
+	const auto &shareCaller = caller.lock();
+	if(!shareCaller)    return RetType();
 
-    std::cout << __FUNCTION__ << "(weak):" << __LINE__ << ' ';
-    return (shareCaller.get()->*callable)(std::forward<Args>(args)...);
+	std::cout << __FUNCTION__ << "(weak):" << __LINE__ << ' ';
+	return (shareCaller.get()->*std::forward<Callable>(callable))(std::forward<Args>(args)...);
 }
 
-template <typename Caller, typename Callable, typename ...Args>
-auto SafeCall(std::shared_ptr<Caller> caller, Callable callable, Args ...args) -> decltype(auto)
+	template <typename Caller, typename Callable, typename ...Args>
+auto SafeCall(std::shared_ptr<Caller> caller, Callable&& callable, Args&&...args) -> decltype(auto)
 {
-    using RetType = decltype((std::declval<Caller>().*callable)(std::forward<Args>(args)...));
+	using RetType = decltype((std::declval<Caller>().*std::forward<Callable>(callable))(std::forward<Args>(args)...));
 
-    if(!caller)    return RetType{};
+	if(!caller)    return RetType();
 
-    std::cout << __FUNCTION__ << "(share):" << __LINE__ << ' ';
-    return (caller.get()->*callable)(std::forward<Args>(args)...);
+	std::cout << __FUNCTION__ << "(share):" << __LINE__ << ' ';
+	return (caller.get()->*std::forward<Callable>(callable))(std::forward<Args>(args)...);
 }
-
 
 int main()
 {
-    auto sharedObj {std::make_shared<Test>()};
-    std::weak_ptr<Test> weakObj {sharedObj};
+	auto sharedObj {std::make_shared<Test>()};
+	std::weak_ptr<Test> weakObj {sharedObj};
+
+    std::string str{"hello"};
+    const std::string& cstr = str;
+
+    SafeCall(sharedObj, (void (Test::*)(const std::string&))&Test::testoverload1, str);
+    SafeCall(sharedObj, (void (Test::*)(const std::string&))&Test::testoverload1, cstr);
+    SafeCall(sharedObj, (void (Test::*)(std::string&))&Test::testoverload1, str);
+//    SafeCall(sharedObj, (void (Test::*)(std::string&))&Test::testoverload1, cstr);
     
-    for(int i = 0; i < 12; ++i)
-    {
-        std::cout << i << '\n';
-    std::cout << SafeCall(sharedObj, &Test::test, i) << '\n';   
-    std::cout << SafeCall(weakObj, &Test::test, i+1) << '\n';   
-    std::cout << SafeCall(sharedObj.get(), &Test::test, i+2) << '\n';
-            std::cout << '\n';
+	for(int i = 0; i < 12; ++i)
+	{
+		std::cout << i << '\n';
+		std::cout << SafeCall(sharedObj, &Test::testint, i) << '\n';   
+		std::cout << SafeCall(weakObj, &Test::testint, i+1) << '\n';   
+		std::cout << SafeCall(sharedObj.get(), &Test::testint, i+2) << '\n';
+        SafeCall(sharedObj, &Test::testvoid);   std::cout << '\n';
+        SafeCall(weakObj, &Test::testvoid);   std::cout << '\n';
+        SafeCall(sharedObj.get(), &Test::testvoid);   std::cout << '\n';
+        std::cout << std::boolalpha << SafeCall(sharedObj, &Test::testbool) << '\n';
+        std::cout << std::boolalpha << SafeCall(weakObj, &Test::testbool) << '\n';
+        std::cout << std::boolalpha << SafeCall(sharedObj.get(), &Test::testbool) << '\n';
+		std::cout << '\n';
 
-    if(i == 7)
-        sharedObj.reset();
-    }   
+		if(i == 7)
+			sharedObj.reset();
+	}   
 
-    return 0;
+
+	return 0;
 }
